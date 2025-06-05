cd93f8d1de1591cbab21f0fff7ee0a2ead5e978c
Bati <no-reply@batijs.dev>
2025-06-05 10:04:15 +0530

    scaffold Vike app with Bati

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { pages } from "vike-cloudflare";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import vike from "vike/plugin";
+
+export default defineConfig({
+  plugins: [vike(), react({}), tailwindcss(), pages()],
+
+  build: {
+    target: "es2022",
+  },
+
+  resolve: {
+    alias: {
+      "@": new URL("./", import.meta.url).pathname,
+    },
+  },
+});
